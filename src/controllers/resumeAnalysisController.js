dbb1a19bad7ab9e7f57b7b264c7921d8a2cc29cb
TechV013 <vishalkheri013@gmail.com>
2026-09-18 23:44:06 +0530

    A

--- src/controllers/resumeAnalysisController.js
+++ src/controllers/resumeAnalysisController.js
@@ -54,7 +54,7 @@ async function analyzeResumeController(req, res) {
     res.json(analysis);
   } catch (error) {
     console.error(error);
-    res.status(500).json({ message: "Unable to analyze resume" });
+    res.status(500).json({ message: error.message || "Unable to analyze resume" });
   }
 }
 
